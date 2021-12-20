611c1be0956bd701d8a41af51f4bc69743e3ff21
dyl <351251178@qq.com>
2021-12-19 17:24:00 -0800

    successfully write and query

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app=express()
 const mongoose = require("mongoose")
 const ejs = require("ejs")
-mongoose.connect('mongodb://172.21.2.236:27017/190110910204');
+var queryres = ""
+mongoose.connect('mongodb://localhost/test');
+// mongoose.connect('mongodb://172.21.2.236:27017/190110910204');
 const schema = {
     name: String,
     age: Number,
@@ -10,20 +12,35 @@ const schema = {
 }
 
 const mydata = mongoose.model('cat1s', schema);
-// const kitty1 = new mydata({ name: 'testZildjian2' });
-// kitty1.save()
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     // res.send(req.query)
-    console.log(req.query)
     // const kitty1 = new mydata({ name: req.query.first,health: req.query.second });
     // kitty1.save()
     // ejs.renderFile(filename, data, options, function(err, str){
     //     // str => 输出渲染后的 HTML 字符串
     // });
-    ejs.renderFile("result.html",{returnVal:"success"},(err,str)=>{
-        res.send(str)
-    })
+    if(req.query.submit1=="Write"){
+        if(req.query!=""){
+            //插入
+            kitty1 = new mydata({ name: req.query.first,health: req.query.second});
+            kitty1.save()
+            ejs.renderFile("result.html",{returnVal:"write successfully"},(err,str)=>{
+                res.send(str)
+            })
+        }
+    }
+    if(req.query.submit1=="Read"){
+        if(req.query!=""){
+            //查询
+            mydata.find({name:req.query.first},(err,data)=>{
+                ejs.renderFile("result.html",{returnVal:"successful query:"+data[0]._doc.health},(err,str)=>{
+                    res.send(str)
+                    console.log(data[0]._doc.health)
+                })
+            })
+        }
+    }
 })
 app.listen(10204)
