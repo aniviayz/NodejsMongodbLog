60e6b79b7baed0aa6d7174059412df2f42acc38d
dyl <351251178@qq.com>
2021-12-25 23:49:00 -0800

    reg and login successfully

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -8,9 +8,18 @@ mongoose.connect('mongodb://localhost/test');
 const schema = {
     name: String,
     age: Number,
-    health: String
+    health: String,
+
 }
 
+const schema_user = {
+    username: String,
+    userpwd: String,
+    admin: Boolean 
+
+}
+//定义模型
+const userdata = mongoose.model('users', schema_user);
 const mydata = mongoose.model('cat1s', schema);
 
 app.use('/',express.static('public'))
@@ -31,16 +40,89 @@ app.get("/input",(req,res)=>{
             })
         }
     }
+
     if(req.query.submit1=="Read"){
         if(req.query!=""){
             //查询
             mydata.find({name:req.query.first},(err,data)=>{
-                ejs.renderFile("result.html",{returnVal:"successful query:"+data[0]._doc.health},(err,str)=>{
-                    res.send(str)
-                    console.log(data[0]._doc.health)
-                })
+                //判断是否查询成功
+                if(data == 0){
+                    ejs.renderFile("result.html",{returnVal:"bad query:"+"请检查你的输入：查无此人"},(err,str)=>{
+                        res.send(str)
+                    })
+                }
+                else{
+                    ejs.renderFile("result.html",{returnVal:"successful query:"+data[0]._doc.health},(err,str)=>{
+                        res.send(str)
+                        console.log(data[0]._doc.health)
+                        console.log(login_code)
+                    })
+                }
             })
         }
     }
+
+    if(req.query.submit1=="Login"){
+        if(req.query!=""){
+            //查询
+            userdata.find({username:req.query.username},(err,data)=>{
+                if(data == 0){
+                    ejs.renderFile("result.html",{returnVal:"bad query:"+"请检查你的输入：查无此人"},(err,str)=>{
+                        res.send(str)
+                    })
+                }
+                else if(data[0]._doc.userpwd == req.query.userpwd){
+                    if(data[0]._doc.admin == true){queryres = "你是管理员"}
+                    else {queryres+ "你是普通用户"}
+                    
+                    console.log(login_code)
+                    var login_code = 1
+                    ejs.renderFile("result.html",{returnVal:"登录成功"+queryres},(err,str)=>{
+                        res.send(str)
+                    })
+                }
+            })
+        }
+    }
+
+    if(req.query.submit1=="Register"){
+        if(req.query!=""){
+            // 注册
+            // 查询部分
+            userdata.find({username:req.query.username},(err,data)=>{
+            if(data == 0){
+                //注册部分
+                console.log(data[0])
+                console.log(req.query.userpwd1,req.query.userpwd2)
+                if(req.query.userpwd1 === req.query.userpwd2){
+                    user1 = new userdata({ 
+                        username: req.query.username,
+                        userpwd: req.query.userpwd1,
+                        admin: false });
+                    
+                    console.log(login_code)    
+                    user1.save()
+                    ejs.renderFile("result.html",{returnVal:"Register successfully"},(err,str)=>{
+                    res.send(str)
+                })
+                }
+                else if(req.query.userpwd1 != req.query.userpwd2){
+                    ejs.renderFile("public/register.html",{returnVal:"两次输入的密码不一致，请重试"},(err,str)=>{
+                        res.send(str)
+                    })
+                }
+                
+            }
+            else if(data[0]._doc.username == req.query.username){
+                    ejs.renderFile("public/register.html",{returnVal:"用户已存在"},(err,str)=>{
+                        res.send(str)
+                    })
+                }
+            })
+            
+            
+        }
+    }
+
 })
 app.listen(10204)
