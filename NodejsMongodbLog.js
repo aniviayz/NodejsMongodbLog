5d2fbf5854dc2ebfd0194497ddda1c7678071e83
dyl <351251178@qq.com>
2021-12-27 08:44:02 -0800

    session admin successfully

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -43,14 +43,23 @@ app.get("/input",(req,res)=>{
     //     // str => 输出渲染后的 HTML 字符串
     // });
     if(req.query.submit1=="Write"){
+        //判断是否登录
         if(req.session.userinfo){
+            //如果输入不为空
             if(req.query!=""){
                 //插入
-                pet1 = new petdata({ name: req.query.first,health: req.query.second,hostname:req.query.third});
-                pet1.save()
-                ejs.renderFile("result.html",{returnVal:"write successfully"},(err,str)=>{
+                if(req.session.adminco == true){
+                    pet1 = new petdata({ name: req.query.first,health: req.query.second,hostname:req.query.third});
+                    pet1.save()
+                    ejs.renderFile("result.html",{returnVal:"write successfully"},(err,str)=>{
                     res.send(str)
                 })
+                }
+                else{
+                    ejs.renderFile("result.html",{returnVal:"您不是管理员！"},(err,str)=>{
+                        res.send(str)
+                    })
+                }
             }
         }
         else {
@@ -62,22 +71,23 @@ app.get("/input",(req,res)=>{
 
     if(req.query.submit1=="Read"){
         if(req.query!=""){
+            //登录判断
             if(req.session.userinfo){
-                //查询
-            petdata.find({name:req.query.first},(err,data)=>{
-                //判断是否查询成功
-                if(data == 0){
-                    ejs.renderFile("result.html",{returnVal:"bad query:"+"请检查你的输入：查无此人"},(err,str)=>{
-                        res.send(str)
+                    //查询
+                    petdata.find({name:req.query.first},(err,data)=>{
+                        //判断是否查询成功
+                        if(data == 0){
+                            ejs.renderFile("result.html",{returnVal:"bad query:"+"请检查你的输入：查无此人"},(err,str)=>{
+                                res.send(str)
+                            })
+                        }
+                        else{
+                            ejs.renderFile("result.html",{returnVal:"successful query:"+data[0]._doc.health},(err,str)=>{
+                                res.send(str)
+                            })
+                        }
                     })
-                }
-                else{
-                    ejs.renderFile("result.html",{returnVal:"successful query:"+data[0]._doc.health},(err,str)=>{
-                        res.send(str)
-                        console.log(data[0]._doc.health)
-                    })
-                }
-            })
+            
             }
             else {
                 ejs.renderFile("result.html",{returnVal:"您当前并未登录"},(err,str)=>{
@@ -87,6 +97,7 @@ app.get("/input",(req,res)=>{
         }
     }
 
+    //登录操作
     if(req.query.submit1=="Login"){
         if(req.query!=""){
             //查询
@@ -104,11 +115,11 @@ app.get("/input",(req,res)=>{
                     }
                     else if(data[0]._doc.userpwd == req.query.userpwd){
                         req.session.userinfo = req.query.username
+                        req.session.adminco = data[0]._doc.admin
+                        console.log(req.session.adminco)
                         if(data[0]._doc.admin == true){queryres = "你是管理员"}
                         else {queryres+ "你是普通用户"}
                         
-                        console.log(login_code)
-                        var login_code = 1
                         ejs.renderFile("result.html",{returnVal:"登录成功"+queryres},(err,str)=>{
                             res.send(str)
                         })
@@ -140,8 +151,7 @@ app.get("/input",(req,res)=>{
                         username: req.query.username,
                         userpwd: req.query.userpwd1,
                         admin: false });
-                    
-                    console.log(login_code)    
+                     
                     user1.save()
                     ejs.renderFile("result.html",{returnVal:"Register successfully"},(err,str)=>{
                     res.send(str)
@@ -169,7 +179,7 @@ app.get("/input",(req,res)=>{
         req.session.destroy(function(err){
             console.log(err);
         })
-        ejs.renderFile("result.html",{returnVal:"登出成功！"},(err,str)=>{
+        ejs.renderFile("public/index.html",{returnVal:"登出成功！"},(err,str)=>{
             res.send(str)
         })
     }
